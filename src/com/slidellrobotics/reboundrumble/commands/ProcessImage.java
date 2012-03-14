d852bee436c66dfac5f81b01f102c1cabfaf45fb
Gus Michel <gixxygamma@gmail.com>
2012-03-14 09:29:00 -0500

    added back relativeSetpoint for LazySusan and reset of StepNo.

--- src/com/slidellrobotics/reboundrumble/commands/ProcessImage.java
+++ src/com/slidellrobotics/reboundrumble/commands/ProcessImage.java
@@ -57,63 +57,7 @@ public class ProcessImage extends CommandBase {
 
     // Called just before this Command runs the first time
     protected void initialize() {
-        /*try {
-            TrackingCamera.pic = camera.getImageFromCamera();
-            //Declares pic variable
-            //System.out.println("Got image");
-            TrackingCamera.totalWidth = TrackingCamera.pic.getWidth();
-            TrackingCamera.totalHeight = TrackingCamera.pic.getHeight();
-
-            //System.out.println("Threshold");
-            TrackingCamera.thresholdHSL = TrackingCamera.pic.thresholdHSL(150, 185, 244, 255, 2, 20);      //Sets a Blue light threshold
-            
-            System.out.println("Checkpoint 1");
-            
-            //System.out.println("Removing Small Objects");
-            //TrackingCamera.bigObjectsImage = TrackingCamera.thresholdHSL.removeSmallObjects(false, 1);
-            
-            //System.out.println("Convex");
-            TrackingCamera.convexHullImage = TrackingCamera.thresholdHSL.convexHull(false);        //Fills in the bounding boxes for the targets            
-
-            System.out.println("Checkpoint Alpha");
-            
-           // System.out.println("Bounding Box Criteria");
-            TrackingCamera.boundImage = TrackingCamera.convexHullImage.particleFilter(TrackingCamera.cc);
-            
-            System.out.println("Checkpoint Bravo");
-            
-            //TODO: Ordered?
-            TrackingCamera.reports = TrackingCamera.boundImage.getOrderedParticleAnalysisReports();        //Sets "reports" to the nuber of particles
-            
-            System.out.println("Checkpoint Charlie");
-            
-            System.out.println("Reports: "+TrackingCamera.reports.length);
-        } catch (NIVisionException ex) {
-            System.out.println(ex);
-        } catch (Exception ex) {
-            System.out.println(ex);
-        }
-
-        System.out.println("Checkpoint 2");
-        
-        try {
-            if (TrackingCamera.pic != null) {
-                TrackingCamera.pic.free();
-            } 
-            if (TrackingCamera.convexHullImage != null) {
-                TrackingCamera.convexHullImage.free();
-            } 
-            if (TrackingCamera.thresholdHSL != null) {
-                TrackingCamera.thresholdHSL.free();
-            }
-            if (TrackingCamera.boundImage != null) {
-                TrackingCamera.boundImage.free();
-            }
-        } catch (Exception ex) {
-            System.out.println("Memory: "+ex);
-        }
-        System.out.println("Checkpoint 3");
-        */
+        stepNo = 1;
     }
 
     // Called repeatedly when this Command is scheduled to run
